fc41b49e6e5b7581a822b1462fa534da964ea707
dachieu2k2 <hieutedam2k2@gmail.com>
2022-06-05 09:46:37 +0700

    add models, add shadows, background, lights

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import "./App.css";
+import Park from "./Components/Park";
+import * as THREE from "three";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas id="three-container" shadows>
+      <color attach={"background"} args={["#9A5DE6"]} />
+      <React.Suspense fallback={null}>
+        <OrbitControls makeDefault />
+        <PerspectiveCamera makeDefault position={[2, 2, 2]} />
+
+        <Park scale={0.2} />
+        {/* Light */}
+        <primitive object={new THREE.AxesHelper(10)} />
+
+        <ambientLight args={["#9A5DE6", 0.5]} />
+        {/* <rectAreaLight position={[0, 2, 0]} args={["#9A5DE6", 2, 3, 3]} /> */}
+        <pointLight castShadow position={[0, 1, 0]} args={["#FF8666", 4, 3]} />
+        {/* <rectAreaLight /> */}
+      </React.Suspense>
+    </Canvas>
   );
 }
 
